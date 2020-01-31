9223a3d956bdbc7c4a403fbc6947fb4f171a97b5
Anthony Muli <mulianthony561@gmail.com>
2020-02-01 02:36:53 +0300

    created a request.py file to in order to make requests to the API

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ class config:
     general configuration parent class
     '''
     pass
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q=bitcoin&from=2019-12-31&sortBy=publishedAt&apiKey=API_KEY'
 
 class ProdConfig(config):
     '''
---
+++ app/request.py
@@ -0,0 +1,4 @@
+from app import app
+
+#Getting API key
+api_key = app.config['NEWS_API_KEY']
